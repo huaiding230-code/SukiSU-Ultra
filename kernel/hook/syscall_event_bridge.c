f6fec7b3e1fab50e3b7a78455171f3e701726373
ELF <huaiding230@gmail.com>
2026-09-26 23:37:36 +0800

    Update syscall_event_bridge.c

--- kernel/hook/syscall_event_bridge.c
+++ kernel/hook/syscall_event_bridge.c
@@ -118,8 +118,8 @@ static long __nocfi ksu_hook_execve_common(int orig_nr, const struct pt_regs *re
             pr_err("adb root failed: %ld\n", ret);
         }
     } else if (ksu_su_compat_enabled) {
-        ret = execveat ? ksu_handle_execveat_sucompat(NULL, filename_user, NULL, NULL, NULL) :
-                 ksu_handle_execve_sucompat(NULL, filename_user, NULL, NULL, NULL);
+        ret = execveat ? ksu_handle_execveat_sucompat(NULL, (void *)filename_user, NULL, NULL, NULL) :
+                 ksu_handle_execve_sucompat(NULL, (void *)filename_user, NULL, NULL, NULL);
         ksu_sulog_emit_pending(pending_root_execve, ret, GFP_KERNEL);
         return ret;
     }
